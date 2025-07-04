59d4c94d7c664f560613f24568c187bbc6ff8c6f
wassimmejri <mejri.wassim@esprit.tn>
2025-07-04 12:27:10 +0100

    second

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const app = express()
 const port = 3001
+const mongoose = require('mongoose');
+
 
 app.get('/', (req, res) => {
   res.sendFile("./Views/home.html",{root: __dirname})
 })
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`)
-})
+
+
+mongoose.connect('mongodb+srv://mejriwassim:tBPKk0k575s6jEXd@cluster0.tkj9mp3.mongodb.net/all-data?retryWrites=true&w=majority&appName=Cluster0')
+. then( () => {
+  app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`)
+  })
+} )
+.catch( (err) => {console.log(err)} );
